c16e5f6736f64dad59625b6b5339ec325c288c1c
mehtajaghvi <jaghvi11@gmail.com>
2012-07-03 16:24:57 +0530

    sb with ubuntu color

--- demo/vt100.js
+++ demo/vt100.js
@@ -1072,7 +1072,7 @@ VT100.prototype.initializeElements = function(container) {
   this.numScrollbackLines      = 0;
   this.top                     = 0;
   this.bottom                  = 0x7FFFFFFF;
-  this.scale                   = 0.57777777;
+  this.scale                   = 0.57;
   this.resizer();
   this.focusCursor();
   this.input.focus();
